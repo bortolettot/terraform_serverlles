9e426b3e0c246b1f369ae7a8f2bde2e8393c8e32
Thiago Bortoletto <bortolettot@gmail.com>
2022-05-01 22:11:48 -0300

    .

---
+++ terraform/infra/users/iam-role-register.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_role" "register_iam_role" {
+  name = "${var.environment}-register-iam-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+}
+
+resource "aws_ssm_parameter" "register_iam_role" {
+  name = "${var.environment}-register-iam-role"
+  type = "String"
+  value = "${aws_iam_role.register_iam_role.arn}"
+}
