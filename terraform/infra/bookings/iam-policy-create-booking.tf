df25738d86403452e9d609da34caf536480e1b06
Thiago Bortoletto <bortolettot@gmail.com>
2022-05-23 00:05:43 -0300

    next

---
+++ terraform/infra/bookings/iam-policy-create-booking.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_policy" "create_booking_policy" {
+   name        = "${var.environment}-create-booking-policy"
+ 
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "dynamodb:PutItem",
+        ]
+        Effect   = "Allow"
+        Resource = "${aws_dynamodb_table.bookings.arn}"
+      },
+      {
+          "Effect": "Allow",
+          "Action": [
+              "logs:CreateLogGroup",
+              "logs:CreateLogStream",
+              "logs:PutLogEvents"
+            ],
+           
+            "Resource": "*"
+        }
+    ]
+  })
+}
