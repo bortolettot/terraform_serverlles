61b855015d0a8b98869557dd261f73dd835b01bd
Thiago Bortoletto <bortolettot@gmail.com>
2022-05-22 22:51:53 -0300

    Next

---
+++ terraform/infra/users/iam-role-login.tf
@@ -0,0 +1,26 @@
+resource "aws_iam_role" "login_iam_role" {
+  name = "${var.environment}-login-iam-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+}
+
+resource "aws_ssm_parameter" "login_iam_role" {
+  name = "${var.environment}-login-iam-role"
+  type = "String"
+  value = "${aws_iam_role.login_iam_role.arn}"
+}
